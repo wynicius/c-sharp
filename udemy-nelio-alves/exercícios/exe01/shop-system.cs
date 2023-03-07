533e4dc10d24cec340393684b93361fb88fbb682
wynicius <wynicius@gmail.com>
2023-03-07 14:26:38 -0300

    Reorganized the repository

---
+++ udemy-nelio-alves/exercícios/exe01/shop-system.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+System.Console.WriteLine("Digite o código da peça 1: ");
+    int code1 = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Digite quantas peças 1 serão : ");
+    int hMany1 = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Digite o valor da peça 1: ");
+    double value1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double fValue1 = hMany1 * value1;
+
+System.Console.WriteLine("Digite o cógido da peça 2: ");
+    int code2 = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Digite quantas peças 2 serão: ");
+    int hMany2 = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Digite o valor da peça 2: ");
+    double value2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double fValue2 = hMany2 * value2;
+
+double payment = (fValue1 + fValue2);
+
+System.Console.WriteLine(fValue1);
+System.Console.WriteLine(fValue2);
+
+System.Console.WriteLine($"VALOR A PAGAR: R$ {payment:F2}");
