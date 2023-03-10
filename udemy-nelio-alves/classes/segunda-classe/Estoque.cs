9c17320fd48a145173b1736fcc97ff313d7c168f
wynicius <wynicius@gmail.com>
2023-03-10 18:10:45 -0300

    continuing on classes studies

---
+++ udemy-nelio-alves/classes/segunda-classe/Estoque.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Globalization;
+
+public class Estoque {
+    public string Nome;
+
+    public double Preco;
+
+    public int Quantidade;
+
+    public double ValorTotalEmEstoque() {
+        return Preco * Quantidade;
+    }
+
+    public void AdicionarProdutos(int quantidade) {
+        Quantidade += quantidade;
+    }
+
+    public void RemoverProdutos(int quantidade) {
+        Quantidade -= quantidade;
+    }
+
+    public override string ToString() {
+        return $"{Nome}, $ {Preco.ToString("F2", CultureInfo.InvariantCulture)}, {Quantidade} unidades, Total: $ {ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)}";
+    }
+
+
+}
---
+++ udemy-nelio-alves/classes/segunda-classe/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+Estoque X = new Estoque();
+
+System.Console.WriteLine("Entre os dados do produto:");
+System.Console.Write("Nome: ");
+    X.Nome = Console.ReadLine();
+
+System.Console.Write("Preço: ");
+    X.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+System.Console.WriteLine("Quantidade no estoque: ");
+    X.Quantidade = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Dados do produto: " + X);
+
+System.Console.Write("Digite o número de produtos a serem adicionados ao estoque: ");
+int qte = int.Parse(Console.ReadLine());
+X.AdicionarProdutos(qte);
+
+System.Console.WriteLine($"Dados atualizados: {X}");
+
+System.Console.Write("Digite o número de produtos a serem removidos ao estoque: ");
+qte = int.Parse(Console.ReadLine());
+X.RemoverProdutos(qte);
+
+System.Console.WriteLine($"Dados atualizados: {X}");
