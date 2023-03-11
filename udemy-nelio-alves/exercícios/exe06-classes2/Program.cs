79f2e5bcfb1bb178ac9e0a53f18bb5041254cc4a
wynicius <wynicius@gmail.com>
2023-03-10 21:01:04 -0300

    Getting the Pump

---
+++ udemy-nelio-alves/exercícios/exe06-classes2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+class Program {
+    static void Main(string[] args) {
+        Retangulo X = new Retangulo();
+
+        Console.WriteLine("Entre a largura e a altura do retângulo: ");
+        System.Console.Write("Largura: ");
+        X.Largura = double.Parse(System.Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        System.Console.Write("Altura: ");
+        X.Altura = double.Parse(System.Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        System.Console.WriteLine($"ÁREA: {X.Area().ToString("F2", CultureInfo.InvariantCulture)}");
+        System.Console.WriteLine($"PERÍMETRO: {X.Perimetro().ToString("F2", CultureInfo.InvariantCulture)}");
+        System.Console.WriteLine($"DIAGONAL: {X.Diagonal().ToString("F2", CultureInfo.InvariantCulture)}");
+    }
+}
