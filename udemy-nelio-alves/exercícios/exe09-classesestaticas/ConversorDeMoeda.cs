2f726612b31a180dc8f4aa4d1f08ac3a54f324e5
wynicius <wynicius@gmail.com>
2023-03-16 12:56:53 -0300

    Static Classes

---
+++ udemy-nelio-alves/exercícios/exe09-classesestaticas/ConversorDeMoeda.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Globalization;
+
+class CambioDeMoeda {
+
+    public static double Iof = 0.06; // IOF de 6%
+    public static double DolarParaReal(double cotacao, double qntDolar) {
+        double total = ((cotacao * Iof) + cotacao) * qntDolar;
+        return total;
+    }
+}
---
+++ udemy-nelio-alves/exercícios/exe09-classesestaticas/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+System.Console.Write("Qual a cotação do dólar? ");
+double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+System.Console.Write("Quantos dólares você vai comprar? ");
+double qntDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+// Está sendo usada a classe estática CambioDeMoeda. A diferença é que não há necessidade de referenciação, nem instanciação.
+
+System.Console.WriteLine("Valor a ser pago em reais = " 
+    + CambioDeMoeda.DolarParaReal(cotacao,qntDolar).ToString("N", CultureInfo.GetCultureInfo("pt-BR")));
+
+// Para formatar o número com o separador de milhar, você pode usar a string "N" juntamente com a cultura "pt-BR" (ou qualquer outra cultura que use o ponto como separador de milhar).
+
