6f1092692f8df10d917b9a7984b6dfb39cf81eeb
wynicius <wynicius@gmail.com>
2023-04-03 22:47:39 -0300

    exercising more on matrix

--- udemy-nelio-alves/exercicios/13_matriz-m-n/Program.cs
+++ udemy-nelio-alves/exercicios/13_matriz-m-n/Program.cs
@@ -14,13 +14,14 @@ int n = int.Parse(Console.ReadLine());
 int[,] mat = new int[m,n];
 
 for (int i = 0; i < m; i++) {
-    Console.Write("Enter the " + (i+1) + "º row's values: ");
+    Console.Write("Enter the values for the " + (i+1) + "th row, separated by spaces: ");
     string[] values = Console.ReadLine().Split(' ');
 
     for (int j = 0; j < n; j++) {
         mat[i, j] = int.Parse(values[j]);
     }
-}
+}    
+Console.WriteLine();
 
 Console.Write("Enter a int that is in the matrix: ");
 int x = int.Parse(Console.ReadLine());
@@ -28,10 +29,19 @@ int x = int.Parse(Console.ReadLine());
 for (int i = 0; i < m; i++) {
     for (int j = 0; j < n; j++) {
         if (mat[i,j] == x) {
-            Console.WriteLine($"Position (${i},${j}):");
-            Console.WriteLine($"Left: ${mat[i,j]}"); 
-            Console.WriteLine($"Right: ${mat[i,j]}"); 
-            Console.WriteLine($"Down: ${mat[i,j]}"); 
+            Console.WriteLine($"Position ({i},{j}):");
+            if (j > 0) {
+                Console.WriteLine("Left: " + mat[i, j - 1]);
+            }
+            if (i > 0) {
+                Console.WriteLine("Up: " + mat[i - 1, j]);
+            }
+            if (j < n - 1) {
+                Console.WriteLine("Right: " + mat[i, j + 1]);
+            }
+            if (i < m - 1) {
+                Console.WriteLine("Down: " + mat[i + 1, j]);
+            } 
         }
     }
 }
