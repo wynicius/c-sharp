429670d7781fdbc1c4f3e0ae1c9f9685711e2eda
wynicius <wynicius@gmail.com>
2023-04-10 19:12:35 -0300

    debug

--- udemy-nelio-alves/vetores/modificadores/params/Program.cs
+++ udemy-nelio-alves/vetores/modificadores/params/Program.cs
@@ -1,16 +1,16 @@
-﻿using Course;
+﻿using System;
 
 int result = Calculator.Sum(10, 20, 30, 40); // com a inserção de params, passar parâmetros se torna mais fácil
 
-namespace Course {
-    class Calculator {
-        public static int Sum(params int[] numbers) { // inserção de params
+class Calculator {
+    public static int Sum(params int[] numbers) // inserção de params
+    {
         int sum = 0;
-        for (int i = 0; i < numbers.Length; i++) {
-            sum += numbers[i];
-        }
+        for (int i = 0; i < numbers.Length; i++) 
+            {
+                sum += numbers[i];
+            }
         return sum;
-        }
     }
 }
 
