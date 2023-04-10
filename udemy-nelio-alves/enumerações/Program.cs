8e0990ac1a63d177d14930987954cc697bf0aa14
wynicius <wynicius@gmail.com>
2023-04-10 18:27:11 -0300

    debugged

---
+++ udemy-nelio-alves/enumerações/Program.cs
@@ -0,0 +1,22 @@
+﻿using Course.Entities;
+using Course.Entities.Enums;
+using System;
+
+
+Order order = new Order
+{
+    Id = 1080,
+    Moment = DateTime.Now,
+    Status = OrderStatus.PendingPayment
+};
+
+Console.WriteLine(order);
+
+string txt = OrderStatus.PendingPayment .ToString();
+
+System.Console.WriteLine(txt);
+
+OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+Console.WriteLine(os);
+Console.WriteLine(txt);
--- udemy-nelio-alves/vetores/modificadores/params/Program.cs
+++ udemy-nelio-alves/vetores/modificadores/params/Program.cs
@@ -1,10 +1,12 @@
-﻿int result = Calculator.Sum(10, 20, 30, 40); // com a inserção de params, passar parâmetros se torna mais fácil
+﻿using Course;
+
+int result = Calculator.Sum(10, 20, 30, 40); // com a inserção de params, passar parâmetros se torna mais fácil
 
 namespace Course {
     class Calculator {
         public static int Sum(params int[] numbers) { // inserção de params
         int sum = 0;
-        for (int i=0; i<numbers.Length; i++) {
+        for (int i = 0; i < numbers.Length; i++) {
             sum += numbers[i];
         }
         return sum;
